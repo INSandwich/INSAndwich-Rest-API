66d06513701516c349fe98e9f8e763985219cebe
Alexandre <alexandre.dufeil@insa-lyon.fr>
2017-04-05 11:51:59 +0200

    Added products.js

---
+++ api/controllers/products.js
@@ -0,0 +1,34 @@
+var sqlite3 = require('sqlite3').verbose();
+var db = new sqlite3.Database('insandwich.db');
+
+var products = {
+
+  getAll: function(req, res) {
+
+  },
+
+  getOne: function(req, res) {
+
+  },
+
+  getCategory: function(req, res) {
+    
+  },
+
+  create: function(req, res) {
+
+  },
+
+  updateProductInfo: function(req, res) {
+
+  },
+
+  delete: function(req, res) {
+
+  },
+
+
+
+
+}
+module.exports = users;
