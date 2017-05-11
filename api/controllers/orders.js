7ca70e98424f9e3fa668f4ec188bcab73f745810
Juleinn <juleintartem@hotmail.fr>
2017-05-11 19:22:51 +0200

    Fixed getByUser and serialized DB queries

--- api/controllers/orders.js
+++ api/controllers/orders.js
@@ -54,38 +54,45 @@ var orders = {
     if(req.query.pageSize != null) pageSize = req.query.pageSize;
     if(req.query.pageNumber != null) pageNumber = req.query.pageNumber;
 
-    db.all("SELECT COUNT(*) as count from Commands Where User_Id = ? and Is_Paid != 0", req.params.id,
-      function(e, r){
-        if((r.length !=0) && ( e == null)){
-          var itemCount = r[0].count;
-          pageCount = roundUp(itemCount/pageSize, 1);
-          //console.log("PageCount = ",pageCount);
+    db.serialize(function(){
+      db.all("SELECT COUNT(*) as count from Commands Where User_Id = ? and Is_Paid != 0", req.params.id,
+        function(e, r){
+          if((r.length !=0) && ( e == null)){
+            var itemCount = r[0].count;
+            pageCount = roundUp(itemCount/pageSize, 1);
+            //console.log("PageCount = ",pageCount);
 
-        }else if(r.length == 0){
-          res.status(500).json({error: "Couldn't get Items count", detail: "No items retrieved."}).end();
-        }else{
-          res.status(500).json({error: "Couldn't get Items count", detail: e}).end();
+          }else if(r.length == 0){
+            res.status(500).json({error: "Couldn't get Items count", detail: "No items retrieved."}).end();
+          }else{
+            res.status(500).json({error: "Couldn't get Items count", detail: e}).end();
+          }
         }
-      }
-    );
+      );
 
-    db.all("select sum(Command_Lines.Amount) as total,\
-    sum(Command_Lines.Amount * Products.Price) as totalPrice,\
-    Commands.*\
-    from Commands, Command_Lines, Products\
-    where Commands.Id = Command_Lines.Command_Id and Commands.User_Id = ?\
-    and Command_Lines.Product_Id = Products.Id and Commands.Is_Paid != 0 group by Command_Lines.Command_Id\
-    LIMIT ? OFFSET ?;",
-    req.params.id,pageSize, pageNumber*pageSize,
-    function(e, r){
-      res.status(200).json({
-        pageSize: pageSize,
-        pageNumber: parseInt(pageNumber),
-        pageCnt: parseInt(pageCount),
-        items: r
-      }).end();
+      db.all("select sum(Command_Lines.Amount) as total,\
+      sum(Command_Lines.Amount * Products.Price) as totalPrice,\
+      Commands.*\
+      from Commands, Command_Lines, Products\
+      where Commands.Id = Command_Lines.Command_Id and Commands.User_Id = ?\
+      and Command_Lines.Product_Id = Products.Id and Commands.Is_Paid != 0 group by Command_Lines.Command_Id\
+      ORDER BY Commands.Id desc\
+      LIMIT ? OFFSET ?;",
+      req.params.id,pageSize, pageNumber*pageSize,
+      function(e, r){
+        if(e == null)
+        {
+          res.status(200).json({
+            pageSize: pageSize,
+            pageNumber: parseInt(pageNumber),
+            pageCnt: parseInt(pageCount),
+            items: r
+          }).end();
+        }else {
+          res.status(500).json({error: "Unable to get commands"}).end();
+        }
+      });
     });
-
   },
 
 
